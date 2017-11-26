bcdb82e10b0044ec2a43346e3c9123a486240a6a
Clement <clement@garrot.org>
2017-11-26 19:28:37 +0100

    Création des class métiers

---
+++ src/php/Site/Entity/Debat.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cleme
+ * Date: 26/11/2017
+ * Time: 19:25
+ */
+
+namespace Site\Entity;
+
+
+class Debat
+{
+
+}
---
+++ src/php/Site/Repository/Debat.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cleme
+ * Date: 26/11/2017
+ * Time: 19:27
+ */
+
+namespace Site\Repository;
+
+
+class Debat
+{
+
+}
